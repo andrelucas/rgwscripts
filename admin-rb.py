bbeeb2129455115dd1b4782a96ceb912923e4e26
André Lucas <andre@ae-35.com>
2025-09-04 11:35:37 +0000

    Make adminrb a bit easier to use. Add usage.

--- admin-rb.py
+++ admin-rb.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import os
 from rgwadmin import RGWAdmin
 import argparse
 import json
@@ -38,28 +39,56 @@ def delete_bucket(args, rgw):
     return True
 
 
+def usage(args, rgw):
+    try:
+        logging.info(f"Getting usage for uid {args.uid}")
+        result = rgw.get_usage(uid=args.uid, show_entries=True, show_summary=True)
+        print(json.dumps(result, indent=4))
+    except Exception as e:
+        print("Error: {}".format(e))
+        return False
+    return True
+
+
 def main():
     parser = argparse.ArgumentParser(description="RGW Admin bucket delete")
-    parser.add_argument("-a", "--access-key", required=True, help="RGW Access Key")
-    parser.add_argument("-s", "--secret-key", required=True, help="RGW Secret Key")
+    parser.add_argument("-a", "--access-key", help="RGW Access Key")
+    parser.add_argument("-s", "--secret-key", help="RGW Secret Key")
     parser.add_argument("-b", "--bucket", help="Bucket name")
     parser.add_argument("-u", "--uid", help="User ID")
-    parser.add_argument("--host", required=True, help="RGW host")
+    parser.add_argument("-e", "--endpoint", help="RGW endpoint HOST[:PORT]")
     parser.add_argument("--purge", action="store_true", help="Purge bucket")
-    parser.add_argument("--stats", action="store_true", help="Show bucket stats")
+    parser.add_argument("--stats", action="store_true",
+                        help="Show bucket stats")
 
     parser.add_argument(
-        "command", choices=["delete", "get_bucket"], help="Command to execute"
+        "command", choices=["delete", "get_bucket", "usage"], help="Command to execute"
     )
 
     args = parser.parse_args()
+
+    if not args.access_key:
+        if 'AWS_ACCESS_KEY_ID' in os.environ:
+            args.access_key = os.environ['AWS_ACCESS_KEY_ID']
+        else:
+            args.access_key = "0555b35654ad1656d804"
+    if not args.secret_key:
+        if 'AWS_SECRET_ACCESS_KEY' in os.environ:
+            args.secret_key = os.environ['AWS_SECRET_ACCESS_KEY']
+        else:
+            args.secret_key = "h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q=="
+    if not args.endpoint:
+        if 'AWS_ENDPOINT_URL' in os.environ:
+            args.endpoint = os.environ['AWS_ENDPOINT_URL']
+        else:
+            args.endpoint = "localhost:8000"
 
     logging.basicConfig(level=logging.INFO)
 
     rgw = RGWAdmin(
         access_key=args.access_key,
         secret_key=args.secret_key,
-        server=args.host,
+        server=args.endpoint,
         secure=False,
         verify=False,
     )
@@ -73,6 +102,8 @@ def main():
         success = delete_bucket(args, rgw)
     elif args.command == "get_bucket":
         success = get_bucket(args, rgw)
+    elif args.command == "usage":
+        success = usage(args, rgw)
     if success:
         sys.exit(0)
     else:
--- adminrb.sh
+++ adminrb.sh
@@ -9,6 +9,6 @@ sk=h7GhxuBLTrlhVUyxSPUKUV8r/2EI4ngqJxD7iBdBYLhwluN30JaT3Q==
 host="$(hostname -f):8000"
 
 set -e -x
-./racmd.sh caps add --uid="testid" --caps="users=*;buckets=*;metadata=*" >/dev/null
+./racmd.sh caps add --uid="testid" --caps="users=*;buckets=*;metadata=*;usage=*" >/dev/null
 
-./admin-rb.py -a "$ak" -s "$sk" --host "$host" "$@"
+./admin-rb.py -a "$ak" -s "$sk" --endpoint "$host" "$@"
